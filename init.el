128f6ce7f35ac146017aa4e84d6ad0443c9dc1a8
Taice <tai.stanic@gmail.com>
2025-07-26 13:48:10 +0200

    update

--- init.el
+++ init.el
@@ -8,6 +8,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auto-save-default nil)
+ '(custom-safe-themes
+   '("781c6a0e4c6d02811d2727050ae4956f9feccee4b425adf5a198ca6a08854a41"
+     "e27c9668d7eddf75373fa6b07475ae2d6892185f07ebed037eedf783318761d7"
+     default))
  '(make-backup-files nil)
  '(package-selected-packages '(gruber-darker-theme)))
 (custom-set-faces
